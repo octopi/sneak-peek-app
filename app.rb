5f7cb99cf920cbfedae6e8b2e34e3a42ff02f21c
David Hu <d@Davids-MacBook-Pro-2.local>
2013-01-05 13:04:28 -0500

    endpoints

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
 	'sup'
 end
+
+post '/checkinhandler' do
+	'checkin'
+end
+
+get '/venue/:id' do
+	params[:id]
+end
+
+get '/venue/:id/newtip' do
+	'submitting new tip'
+end
