ed0a1207c5d8a9f8c8d90e397e59aab71f76c3b0
David Hu <d@Davids-MacBook-Pro-2.local>
2013-01-05 13:26:23 -0500

    checkinhandler testing

--- app.rb
+++ app.rb
@@ -2,11 +2,14 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
+	puts 'something here'
 	'sup'
 end
 
 post '/checkinhandler' do
-	'checkin'
+	puts '>>> PRINTING CHECKIN'
+	puts params[:checkin]
+	'end of checkin handler'
 end
 
 get '/venue/:id' do
