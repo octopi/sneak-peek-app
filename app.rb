1af5eae3b29e6d16417770973217cc30949dd26e
David Hu <d@Davids-MacBook-Pro-2.local>
2013-01-05 14:02:41 -0500

    more meh

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ end
 post '/checkinhandler' do
 	@venue = params[:checkin]['venue']
 	@checkinparam = params[:checkin]
-	puts "param: #{@checkinparam.inspect}"
+	
 
 	puts "venue: #{@venue.inspect}"
 	puts '>>> VENUE CATEORIES'
